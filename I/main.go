1d16ff18c85ef5047dd5e17ea89fb2dc1af4e71c
Хмелев Евгений <khmelev.ea@kamaz.ru>
2023-02-01 16:23:56 +0300

    I completed

--- I/main.go
+++ I/main.go
@@ -2,61 +2,15 @@ package main
 
 import (
 	"bufio"
-	"container/heap"
 	"fmt"
 	"os"
+	"sort"
 )
 
-// Item - это то, чем мы управляем в приоритетной очереди.
-type Item struct {
-	value    string // Значение элемента; произвольное.
-	priority int    // Приоритет элемента в очереди.
-	// Индекс необходим для обновления
-	// и поддерживается методами heap.Interface.
-	index int // Индекс элемента в куче.
-}
-
-// PriorityQueue реализует heap.Interface и содержит Items.
-type PriorityQueue []*Item
-
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, priority so we use greater than here.
-	// Мы хотим, чтобы Pop давал нам самый высокий,
-	// а не самый низкий приоритет,
-	// поэтому здесь мы используем оператор больше.
-	return pq[i].priority > pq[j].priority
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	item := x.(*Item)
-	item.index = n
-	*pq = append(*pq, item)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	item := old[n-1]
-	old[n-1] = nil  // избежать утечки памяти
-	item.index = -1 // для безопасности
-	*pq = old[0 : n-1]
-	return item
-}
-
-// update изменяет приоритет и значение Item в очереди.
-func (pq *PriorityQueue) update(item *Item, value string, priority int) {
-	item.value = value
-	item.priority = priority
-	heap.Fix(pq, item.index)
+type CPU struct {
+	Energy    int
+	StartTime int
+	TimeExec  int
 }
 
 func main() {
@@ -67,18 +21,31 @@ func main() {
 	var CPUs, tasks int
 	fmt.Fscan(in, &CPUs, &tasks)
 
-	var energyConsump []int
+	var cpuSlice []*CPU
 
 	for i := 0; i < CPUs; i++ {
 		var energy int
 		fmt.Fscan(in, &energy)
-		energyConsump = append(energyConsump, energy)
+		cpuSlice = append(cpuSlice, &CPU{Energy: energy})
 	}
 
+	sort.Slice(cpuSlice, func(i, j int) bool {
+		return cpuSlice[i].Energy < cpuSlice[j].Energy
+	})
+
+	res := 0
 	for i := 0; i < tasks; i++ {
-		var task, sec int
-		fmt.Fscan(in, &task, &sec)
+		var startTime, timeDuration int
+		fmt.Fscan(in, &startTime, &timeDuration)
+		for _, val := range cpuSlice {
+			if startTime-val.StartTime >= val.TimeExec {
+				res += val.Energy * timeDuration
+				val.StartTime = startTime
+				val.TimeExec = timeDuration
+				break
+			}
+		}
 
-		//fmt.Fprintln(out, n+m)
 	}
+	fmt.Fprintln(out, res)
 }
